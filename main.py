46eddd1bffde84f90bd5117a1596aa513a40dcb9
W0rldlyPl4nt <ryan@ryanfnutt.com>
2025-07-17 17:37:10 -0400

    added clock funtion to run the function only evry 1/60th of a second

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+
+        # add clock tick method - pause the loop every 1/60th second
+        # return .tick as seconds to dt variable
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
