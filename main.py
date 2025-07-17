575d51446726e9ab3650c97366c79238ebc1c6ec
W0rldlyPl4nt <ryan@ryanfnutt.com>
2025-07-17 18:34:25 -0400

    add player file to define player triangle to draw

--- main.py
+++ main.py
@@ -3,17 +3,20 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH/2, y= SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
 
         # add clock tick method - pause the loop every 1/60th second
